584b08b944e4cdabb0736dc2febb8046e6d8cc8c
EslamMohamed09 <eslammayo2022@gmail.com>
2024-07-31 14:42:18 +0300

    problem 2

--- js/main.js
+++ js/main.js
@@ -13,4 +13,20 @@ function evenOrOdd(number) {
 // Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
 // Additionally, if the number is negative, return 0.
 
+function sumOfMultiples(number) {
+    if (number < 0) {
+      return 0;
+    }
+  
+    let sum = 0;
+  
+    for (let i = 1; i < number; i++) {
+      if (i % 3 === 0 || i % 5 === 0) {
+        sum += i;
+      }
+    }
+  
+    return sum;
+}
+
 document.write(evenOrOdd(9))
