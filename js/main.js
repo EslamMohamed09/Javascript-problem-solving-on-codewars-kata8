a40d187451679f8c11b7f3a0a0180efd816c17e2
EslamMohamed09 <eslammayo2022@gmail.com>
2024-08-01 14:25:34 +0300

    problem 6

--- js/main.js
+++ js/main.js
@@ -88,3 +88,31 @@ function accum(s){
       return ele.toUpperCase() + ele.toLowerCase().repeat(index);
     }).join('-');
 }
+
+
+/* problem 6 */
+/*
+Given a point in a Euclidean plane (x and y), return the quadrant the point exists in: 1, 2, 3 or 4 (integer). 
+x and y are non-zero integers, therefore the given point never lies on the axes.
+
+Examples
+(1, 2)     => 1
+(3, 5)     => 1
+(-10, 100) => 2
+(-1, -9)   => 3
+(19, -56)  => 4
+*/
+
+function findQuadrant(x, y) {
+  if (x > 0 && y > 0) {
+    return 1; // First quadrant
+  } else if (x < 0 && y > 0) {
+    return 2; // Second quadrant
+  } else if (x < 0 && y < 0) {
+    return 3; // Third quadrant
+  } else if (x > 0 && y < 0) {
+    return 4; // Fourth quadrant
+  }
+}
+
+/* problem 7 */
