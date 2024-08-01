bbe204b6d8128322ba862beb4e8e8e09ddff608e
EslamMohamed09 <eslammayo2022@gmail.com>
2024-08-01 15:23:08 +0300

    problem 7

--- js/main.js
+++ js/main.js
@@ -116,3 +116,28 @@ function findQuadrant(x, y) {
 }
 
 /* problem 7 */
+// Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+function likes(names){
+  const length = names.length;
+
+  if (length === 0){
+      return 'no one likes this';
+  } else if (length === 1) {
+    return `${names} likes this`;
+  } else if(length === 2) {
+    return `${names[0]} and ${names[1]} like this`;
+  } else if(length === 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  } else if (length > 3 ){
+    return `${names[0]}, ${names[1]} and ${length - 2} others like this`;
+  }
+}
+
+/* problem 8 */
