40a90f2fad16a5ee23eac55e81ab67264f962bb9
EslamMohamed09 <eslammayo2022@gmail.com>
2024-08-08 00:12:04 +0300

    problem 15

--- js/main.js
+++ js/main.js
@@ -266,3 +266,33 @@ function returnDay(num){
       return "Wrong, please enter a bumber between 1 and 7";
   }
 }
+
+/* problem 14 */
+// Expressions Matter
+// Given three integers a, b, and c, return the largest number obtained after inserting the operators +, *, and parentheses (). In other words, try every combination of a, b, and c with the operators, without reordering the operands, and return the maximum value.
+
+// Example
+// With the numbers 1, 2, and 3, here are some possible expressions:
+
+// 1 * (2 + 3) = 5
+// 1 * 2 * 3 = 6
+// 1 + 2 * 3 = 7
+// (1 + 2) * 3 = 9
+// The maximum value that can be obtained is 9.
+
+// Notes
+// The numbers are always positive, in the range 1 ≤ a, b, c ≤ 10.
+
+function expressionsMatter(a, b, c) {
+  return Math.max(
+      a + b + c,
+      a + b * c,
+      a * b + c,
+      a * b * c,
+      (a + b) * c,
+      a * (b + c)
+  );
+}
+expressionsMatter(1, 2, 3);  // Output: 9
+
+/* problem 15 */
